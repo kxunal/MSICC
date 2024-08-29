7e5a237746600a5fbd4dc61cd18b7e8971f64a6a
ᴀᴜʀᴀ ⁹⁹⁹⁺ <k76990692@gmail.com>
2024-08-29 19:45:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ERA990/MSICC",
+    "https://github.com/kxunal/Directories",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(

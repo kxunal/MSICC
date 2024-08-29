9993aab07ebcdd53c3cb9c23cb1c4e07a7bc2b8d
ᴀᴜʀᴀ ⁹⁹⁹⁺ <k76990692@gmail.com>
2024-08-29 19:15:07 +0530

    Update userbot.py

--- VenomX/core/userbot.py
+++ VenomX/core/userbot.py
@@ -24,35 +24,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="VIPAss1",
+            name="VenomXAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="VIPAss2",
+            name="VenomXAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="VIPAss3",
+            name="VenomXAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="VIPAss4",
+            name="VenomXAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="VIPAss5",
+            name="VenomXAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
